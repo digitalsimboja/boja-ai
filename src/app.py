fd8b4bd7e275c2c041c378d8783547a4008ea529
Sunday Mgbogu <digitalsimboja@gmail.com>
2023-05-10 11:04:12 +0100

    completes the chatbot app

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import streamlit as st
+from streamlit_chat import message
 import openai
 import pinecone
 
@@ -14,66 +15,42 @@ openai.api_key = os.environ.get("OPENAI_KEY")
 pinecone.init(api_key=os.environ.get("PINECONE_KEY"),
               environment='northamerica-northeast1-gcp')
 
-# Define Pinecone vector index and dimensions
-index_name = "chatbot-index"
-dimensions = 768
-
-# Initialize Pinecone vector index if not exists
-if index_name not in pinecone.list_indexes():
-    pinecone.create_index(name=index_name, dimension=dimensions)
-
-# Define function to generate OpenAI GPT-3 responses
-
 
 def generate_response(prompt):
     response = openai.Completion.create(
         engine="davinci",
         prompt=prompt,
-        max_tokens=2000,
+        max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.5,
     )
-    return response.choices[0].text.strip()
-
-# Define Streamlit app
+    return response.choices[0].text if response.choices else ''
 
 
-def app():
-    st.title("Chatbot")
+st.title("BojaAI Self-service ChatBot")
+if "generated" not in st.session_state:
+    st.session_state["generated"] = []
 
-    # Get user input
-    user_input = st.text_input("You: ")
+if "past" not in st.session_state:
+    st.session_state["past"] = []
 
-    # Convert user input to a float if possible, otherwise use the input as is
-    try:
-        user_input = float(user_input)
-    except ValueError:
-        pass
 
-    # Store user preferences in Pinecone vector index
-    if user_input:
-        # Generate OpenAI GPT-3 response
-        prompt = f"User: {user_input}\nChatbot:"
-        response = generate_response(prompt)
-        if isinstance(user_input, str):
-            id = user_input
-        else:
-            id = str(user_input)
-        vector = [float(x) for x in response.split("\n")[0].split()]
-        pinecone.upsert(index_name=index_name, ids=[id], vectors=[vector])
-        # Retrieve similar user preferences from Pinecone vector index
-        if response == "I don't know.":
-            query = pinecone.query(index_name=index_name,
-                                query_vectors=[vector], top_k=5)
-            if query.ids:
-                st.write("Similar user preferences:")
-                for id in query.ids:
-                    st.write(id)
+def get_text():
+    input_text = st.text_input(
+        "You", "Welcome to BojaAI Self-service", key="user_input")
+    return input_text
 
-        # Display chatbot response
-        st.text_area("Chatbot:", value=response, height=200, max_chars=2048)
 
-# Run Streamlit app
-if __name__ == "__main__":
-    app()
+user_input = get_text()
+
+if user_input:
+    output = generate_response(user_input)
+    st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
+if st.session_state['generated']:
+    for i in range(len(st.session_state['generated'])-1, -1, -1):
+        message(st.session_state["generated"][i], key=str(i))
+        message(st.session_state["past"][i],
+                is_user=True, key=str(i) + '_user')
