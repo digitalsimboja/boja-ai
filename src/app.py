03ee1beaf42b5bcffd261ba5bba5e9118a25fd54
Sunday Mgbogu <digitalsimboja@gmail.com>
2023-05-13 07:19:32 +0100

    updates the model

--- src/app.py
+++ src/app.py
@@ -4,6 +4,10 @@ import streamlit as st
 import openai
 import pinecone
 from streamlit_chat import message
+import spacy
+from spacy.language import Language
+from spacy.tokens import Doc
+
 
 # Load environment variables from .env file
 load_dotenv()
@@ -18,22 +22,26 @@ pinecone.init(api_key=os.environ.get("PINECONE_KEY"),
 
 def generate_response(prompt):
     response = openai.Completion.create(
-        engine="davinci",
+        model="text-davinci-003",
         prompt=prompt,
         max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.5,
     )
-    return response.choices[0].text if response.choices else ''
-
+    if response.choices:
+        first_line = response.choices[0].text
+        return first_line
+    else:
+        return ''
 
 st.title("BojaAI Semantic Search Engine ")
+
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
 
 if "past" not in st.session_state:
-    st.session_state["past"] = [] 
+    st.session_state["past"] = []
 
 
 def get_text():
@@ -46,8 +54,9 @@ user_input = get_text()
 
 if user_input:
     output = generate_response(user_input)
-    st.session_state.generated.append(output)
     st.session_state.past.append(user_input)
+    st.session_state.generated.append(output)
+
 
 if st.session_state['generated']:
     for i in range(len(st.session_state['generated'])-1, -1, -1):
